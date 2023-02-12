9024a2005558c2f9cf2535b1ceeae8bf1b7102c7
mp30028 <mp30028@gmail.com>
2023-02-12 14:06:17 +0000

    draft-3

--- actions/connect-to-remote-server/entrypoint.sh
+++ actions/connect-to-remote-server/entrypoint.sh
@@ -5,4 +5,4 @@ password=$2
 server=$3
 
 # echo "server=$server, username=$username, password=$password"
-command curl -k sftp://$server --user $username:$password
+curl -k sftp://$server --user $username:$password
