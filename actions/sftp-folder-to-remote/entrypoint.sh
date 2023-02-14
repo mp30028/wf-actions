c450f9ee19f2e6f974baa31a44b0d8af245800fb
mp30028 <mp30028@gmail.com>
2023-02-14 15:33:03 +0000

    completed developing sftp action and test workflow

--- actions/sftp-folder-to-remote/entrypoint.sh
+++ actions/sftp-folder-to-remote/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -eu
 # -e -->  exit on error
@@ -10,24 +10,29 @@ port=$3
 ssh_private_key=$4
 local_path=$5
 remote_path=$6
-args=$7
-remote_path_subdir="/staging"
 
-SSH_PRIVATE_KEY_FILE='./id_rsa'
-SFTP_BATCH_FILE='./sftp'
+#echo $username
+#echo $host
+#echo $port
+#echo $ssh_private_key
+#echo $local_path
+#echo $remote_path
+
+SSH_PRIVATE_KEY_FILE=./id_rsa
+SFTP_BATCH_FILE=./sftp_batch
 
 printf "%s" "$ssh_private_key" > $SSH_PRIVATE_KEY_FILE
-sudo chmod 400 $SSH_PRIVATE_KEY_FILE
+chmod 600 $SSH_PRIVATE_KEY_FILE
 
 echo 'Starting SFTP...'
-printf "%s" "put -r $local_path $remote_path$remote_path_subdir" > $SFTP_BATCH_FILE
+printf "%s" "put -r $local_path $remote_path" > $SFTP_BATCH_FILE
 
-sudo ssh -o StrictHostKeyChecking=no -p $port -i $SSH_PRIVATE_KEY_FILE $username@$host mkdir -p $remote_path$remote_path_subdir
+ssh -o StrictHostKeyChecking=no -p $port -i $SSH_PRIVATE_KEY_FILE $username@$host mkdir -p $remote_path
 
-sudo sftp -b $SFTP_BATCH_FILE -P $port $args -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY_FILE $username@$host
+sftp -b $SFTP_BATCH_FILE -P $port -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY_FILE $username@$host
 
-sudo rm $SSH_PRIVATE_KEY_FILE
-sudo rm $SFTP_BATCH_FILE
+rm $SSH_PRIVATE_KEY_FILE
+rm $SFTP_BATCH_FILE
 
 echo 'Transfer completed successfully!'
 exit 0
